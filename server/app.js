4d89193e6c84c0c8012c974b2bd620848cfdd703
Joseph Barsness <joseph.barsness@gmail.com>
2016-12-08 14:20:47 -0600

    initial setup done.

---
+++ server/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var urlEncodedParser = bodyParser.urlencoded({extended:false});
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.listen(port, function(req, res){
+  console.log('server listening on', port);
+}); // end spin up server
+
+// base url
+app.get ('/', function(req, res){
+  console.log('base url hit');
+  res.sendFile(path.resolve('/public/index.html'));
+}); // end base url
+
+//testPost
+app.post('/textPost', urlEncodedParser, function (req, res){
+  console.log('testPost url hit, req.body:', req.body);
+});
+
+
+app.use (express.static('public'));
